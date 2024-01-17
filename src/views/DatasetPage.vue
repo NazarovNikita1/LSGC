ae13f545b18575451d6ed6a25168ee66ffb5b03f
NazarovNikita1 <145589126+NazarovNikita1@users.noreply.github.com>
2024-01-17 21:35:29 +0300

    Update DatasetPage.vue

--- src/views/DatasetPage.vue
+++ src/views/DatasetPage.vue
@@ -1,30 +1,27 @@
 <template>
   <div class="dataset-container">
     <p>
-      In the following links you can find examples for testing your systems:
-      <br />
-      <a :href="getDownloadLink(1)" download>Text 1</a>
-      <br />
-      <a :href="getDownloadLink(2)" download>Text 2</a>
-      <br />
-      <a :href="getDownloadLink(3)" download>Text 3</a>
-      <br />
-      <a :href="getDownloadLink(4)" download>Text 4</a>
-      <br />
-      <a :href="getDownloadLink(5)" download>Text 5</a>
+      In the following links, you can find fiction examples to test your systems:
     </p>
+    <ul>
+      <li>
+        <a href="src/views/datasettext1.txt" download>Text 1</a>
+      </li>
+      <li>
+        <a href="src/views/datasettext2.rtf" download>Text 2</a>
+      </li>
+      <li>
+        <a href="src/views/datasettext3.rtf" download>Text 3</a>
+      </li>
+      <li>
+        <a href="src/views/datasettext4.rtf" download>Text 4</a>
+      </li>
+      <li>
+        <a href="src/views/datasettext5.rtf" download>Text 5</a>
+      </li>
+    </ul>
   </div>
 </template>
-
-<script>
-export default {
-  methods: {
-    getDownloadLink(number) {
-      return `/src/views/big_zachini_for_edition_${number}.txt`;
-    }
-  }
-}
-</script>
 
 <style>
 .dataset-container {
@@ -34,3 +31,4 @@ export default {
   font-size: 1.2em;
 }
 </style>
+
