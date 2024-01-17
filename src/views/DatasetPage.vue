fbf704173fe9536a68f78afcaeb746ee5c5b6c9d
NazarovNikita1 <145589126+NazarovNikita1@users.noreply.github.com>
2024-01-17 19:22:14 +0300

    Update DatasetPage.vue

--- src/views/DatasetPage.vue
+++ src/views/DatasetPage.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="dataset-container">
     <p>
-      In the following links you can find examples for testing your systems. To access the datasets, please
-      <a
-        href="https://raw.githubusercontent.com/NazarovNikita1/LSGC/main/src/views/big_zachini_for_edition.txt"
-        target="_blank"
-      >
-        click here
-      </a>
-      .
+      In the following links you can find examples to test your systems:
+      <br />
+      <a href="/src/views/datasettext1.txt" target="_blank">Text 1</a>
+      <br />
+      <a href="/src/views/datasettext2.rtf" target="_blank">Text 2</a>
+      <br />
+      <a href="/src/views/datasettext3.rtf" target="_blank">Text 3</a>
+      <br />
+      <a href="/src/views/datasettext4.rtf" target="_blank">Text 4</a>
+      <br />
+      <a href="/src/views/datasettext5.rtf" target="_blank">Text 5</a>
     </p>
   </div>
 </template>
