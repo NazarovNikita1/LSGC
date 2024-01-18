9b4b354ffef89c5e47d41b4820e308d6772c5d45
NazarovNikita1 <145589126+NazarovNikita1@users.noreply.github.com>
2024-01-18 19:57:24 +0300

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,11 +3,7 @@
     <h1>Shared Task Overview</h1>
     <p class="program-step">
       Modeling long stories requires many additional abilities compared to short
-      texts, including
-    </p>
-    <ol class="program-step">
-      <li>
-        commonsense reasoning regarding characters' reaction and intention, and
+      texts, including commonsense reasoning regarding characters' reaction and intention, and
         knowledge about physical objects (e.g., "river") and abstract concepts
         (e.g., "irony");
       </li>
