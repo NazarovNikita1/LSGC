eaaa7e62b87eb2204179279aaed2cf7384e34c08
NazarovNikita1 <145589126+NazarovNikita1@users.noreply.github.com>
2024-01-24 20:27:42 +0300

    Update BaselinePage.vue

--- src/views/BaselinePage.vue
+++ src/views/BaselinePage.vue
@@ -4,7 +4,6 @@
       In this <a href="https://colab.research.google.com/drive/1twQTqVUxSUKrgKcvg5v_a4Sw9MWhvpGY?usp=sharing" target="_blank">link</a> you can find a Colab code that generates the fanfictions
     </p>
   </div>
-  <template>
   <div class="baseline-container">
     <p>
       In this <a href="src/views/result_fanfiction.docx" download>link</a> you can find an example of a full fanfic created from the code above
@@ -17,7 +16,7 @@
   z-index: 10;
   padding: 5vh;
   color: white;
-  font-size: 1.2em;
+  font-size: 1.5em;
 }
 </style>
 
