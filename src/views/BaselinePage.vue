a9025ad9feaf6102b5ea51a5d8f2efb2092dba81
NazarovNikita1 <145589126+NazarovNikita1@users.noreply.github.com>
2024-01-24 20:21:45 +0300

    Update BaselinePage.vue

--- src/views/BaselinePage.vue
+++ src/views/BaselinePage.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="baseline-container">
     <p>
-      In this <a href="https://colab.research.google.com/drive/1twQTqVUxSUKrgKcvg5v_a4Sw9MWhvpGY?usp=sharing" target="_blank">link</a> you can find a Colab code that generates the fanfictions:
+      In this <a href="https://colab.research.google.com/drive/1twQTqVUxSUKrgKcvg5v_a4Sw9MWhvpGY?usp=sharing" target="_blank">link</a> you can find a Colab code that generates the fanfictions
+    </p>
+  </div>
+  <template>
+  <div class="baseline-container">
+    <p>
+      In this <a href="https://example.com/path/to/your.pdf" download>link</a> you can find a full fanfiction example
     </p>
   </div>
 </template>
