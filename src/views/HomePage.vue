84cd22dd11a13547811acc5283a6abbff0721a67
NazarovNikita1 <145589126+NazarovNikita1@users.noreply.github.com>
2024-06-19 17:23:21 +0300

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -87,6 +87,9 @@
             DEC 25, 2023 - The Long Story Geeration Challenge website is up and
             running
         </li>
+        <li>
+            JUNE 19, 2024 - Added <a href="https://github.com/nickm197/GAPELMAPER/tree/main" target="_blank">reference implementation</a>
+        </li>
       </ul>
     </div>
   </div>
